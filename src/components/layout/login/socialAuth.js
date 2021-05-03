acf893ea370314ec2853ad8075463598e96c3a12
Daniel Kamar <kamarster@gmail.com>
2021-05-03 03:33:06 +0300

    additional changes

--- src/components/layout/login/socialAuth.js
+++ src/components/layout/login/socialAuth.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
-import {Button, Icon} from 'semantic-ui-react';
+// import {Button, Icon} from 'semantic-ui-react';
 
-const socialButtons = (props) => {
-  const { facebookLogin, googleLogin, twitterLogin } = props;
-  return (
-    <div>
-      <Button color="facebook " onClick={facebookLogin}>
-        <Icon name="facebook" />
-      </Button>
-      <Button color="twitter" onClick={twitterLogin}>
-        <Icon name="twitter" />
-      </Button>
-      <Button color="google plus" onClick={googleLogin}>
-        <Icon name="google" />
-      </Button>
-    </div>
-  );
-};
+// const socialButtons = (props) => {
+//   const { facebookLogin, googleLogin, twitterLogin } = props;
+//   return (
+//     <div>
+//       <Button color="facebook " onClick={facebookLogin}>
+//         <Icon name="facebook" />
+//       </Button>
+//       <Button color="twitter" onClick={twitterLogin}>
+//         <Icon name="twitter" />
+//       </Button>
+//       <Button color="google plus" onClick={googleLogin}>
+//         <Icon name="google" />
+//       </Button>
+//     </div>
+//   );
+// };
 
-socialButtons.propTypes = {
-  facebookLogin: PropTypes.func.isRequired,
-  twitterLogin: PropTypes.func.isRequired,
-  googleLogin: PropTypes.func.isRequired,
-};
+// socialButtons.propTypes = {
+//   facebookLogin: PropTypes.func.isRequired,
+//   twitterLogin: PropTypes.func.isRequired,
+//   googleLogin: PropTypes.func.isRequired,
+// };
 
-export default socialButtons;
+// export default socialButtons;
