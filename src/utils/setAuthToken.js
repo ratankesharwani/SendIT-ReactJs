c2ac44bca55dcc3ecc443f8a23082785c3812853
Daniel Kamar <kamarster@gmail.com>
2021-05-03 03:39:40 +0300

    additional changes

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 
-const setAuthToken = (token) => {
-  if (token) {
-    // Apply to every request
-    axios.defaults.headers.common.Authorization = token;
-  } else {
-    // delete auth header
-    delete axios.defaults.headers.common.Authorization;
-  }
-};
+// const setAuthToken = (token) => {
+//   if (token) {
+//     // Apply to every request
+//     axios.defaults.headers.common.Authorization = token;
+//   } else {
+//     // delete auth header
+//     delete axios.defaults.headers.common.Authorization;
+//   }
+// };
 
-export default setAuthToken;
+// export default setAuthToken;
